50c194d116c36297c7b22695a863b60d68cdcdb7
infinity-Noah <nikhil950833@gmail.com>
2026-05-30 21:56:01 +0545

    auth-connections

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 
-import * as schema from "./scehma";
+import * as schema from "./schema";
 
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
